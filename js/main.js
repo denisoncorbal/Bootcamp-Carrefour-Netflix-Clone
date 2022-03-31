81421910d3ed894f8d9155d47ea5cd3d633cf276
Denison G. Corbal <denison.corbal@gmail.com>
2022-03-31 20:41:05 -0300

    Base com projeto com feature de adição de função nos botões e outros elementos na página

---
+++ js/main.js
@@ -0,0 +1,12 @@
+﻿function assistir(){
+	openInNewTab("https://www.netflix.com/watch/70178217?source=35");
+}
+
+function informacoes(){
+	openInNewTab("https://www.netflix.com/br/title/70178217#section-more-details");
+}
+
+function openInNewTab(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
+}
